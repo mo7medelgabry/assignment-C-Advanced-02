6526be6563c878ec63e710b654004f62d7a8b900
mo7medelgabry <mo7medelgabry@gmail.com>
2024-08-02 04:08:53 +0300

    Add project files.

---
+++ assignment C# Advanced 02/FixedSizeList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_C__Advanced_02
+{
+    internal class FixedSizeList<T>
+    {
+        public T[] Items;
+        public int Count;
+
+        public FixedSizeList(int Capacity)
+        {
+           Items = new T[Capacity];
+            Count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (Count >= Items.Length)
+            {
+                throw new InvalidOperationException("List capacity is Full.");
+            }
+            Items[Count++] = item;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), "Index out of range.");
+            }
+            return Items[index];
+        }
+
+        public int count => Count;
+        public int Capacity => Items.Length;
+
+
+
+
+
+    }
+}
---
+++ assignment C# Advanced 02/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+
+namespace assignment_C__Advanced_02
+{
+    internal class Program 
+
+    {
+        public static void RevArray(ArrayList list)
+        {
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start < end)
+            {
+                // Swap elements at left and right indices
+                object temp = list[start];
+                list[start] = list[end];
+                list[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+            static void Main(string[] args)
+        {
+            #region Part01
+            //Structure : How elements are organized and accessed  
+            //Time Complexity : Big O Notation: Expresses the upper bound of an algorithm's runtime as input size grows.
+            //Business Cases : Problem Domain , Requirements, Constrains, Evaluation  
+
+            #endregion
+
+            #region Part02 Q1
+            ArrayList list = new ArrayList { 1, 2, 3, 4, 5 };
+            RevArray(list); 
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+
+            Console.WriteLine("==================================================");
+            #region Q2
+            ArrayList list2 = new ArrayList { 10, 11, 12, 13, 14 };
+
+            for (int i = 0; i < list2.Count; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(list2[i]);
+                }
+                
+            }
+            #endregion
+            Console.WriteLine("==================================================");
+
+            #region Q3
+            FixedSizeList<int> list3 = new FixedSizeList<int>(3);
+            list3.Add(1);
+            list3.Add(2);
+            list3.Add(3);
+            for (int i = 0;i < list3.Count; i++)
+            {
+                Console.WriteLine(list3.Get(i));
+            }
+
+
+            #endregion
+
+
+
+
+        }
+    }
+}
